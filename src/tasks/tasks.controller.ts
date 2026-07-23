8dd0f8f1826cc610f36c1cafaa4f4ca8273aaf41
Debjani <debjani6ghosh@gmail.com>
2026-07-23 21:18:44 +0530

    feat: implement tasks controller and service with basic CRUD operations

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
-@Controller('tasks')
-export class TasksController {}
+@Controller('tasks') // sets the base route: /tasks
+export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
+
+  @Get()
+  findALl() {
+    return this.tasksService.findAll();
+  }
+
+  @Post()
+  create(@Body() body: { title: string }) {
+    return this.tasksService.create(body.title);
+  }
+}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks: { id: number; title: string }[] = [
+    { id: 1, title: 'Sample Task 1' },
+    { id: 2, title: 'Sample Task 2' },
+  ];
+  private nextId: number = 1;
+
+  findAll(): { id: number; title: string }[] {
+    return this.tasks;
+  }
+
+  create(title: string): { id: number; title: string } {
+    const newTask = { id: this.nextId++, title };
+    this.tasks.push(newTask);
+    return newTask;
+  }
+}
